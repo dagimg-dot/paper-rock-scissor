b08c3e8f7b2a7bbcfb0d1d5ca36b739cfc1168bd
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-20 13:45:17 +0300

    finish basic game prototype

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world")
+calculatorDict = {
+    "rs": 'r',
+    "pr": 'p',
+    "sp": 's',
+    "sr": 'r',
+    "rp": 'p',
+    "ps": 's',
+}
+
+
+def main():
+    p1 = input("Player 1: ")
+    p2 = input("Player 2: ")
+    winner = calculateWinner(p1, p2)
+    print(f"The winner is {winner}")
+
+
+def calculateWinner(p1, p2):
+    choice = p1 + p2
+    return calculatorDict[choice]
+
+
+# Call the main method
+main()
