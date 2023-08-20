29a946e1cf583ae45b8676923b0d6bb6034b7a00
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-20 14:07:39 +0300

    handle invalid input

--- main.py
+++ main.py
@@ -9,10 +9,25 @@ calculatorDict = {
 
 
 def main():
-    p1 = input("Player 1: ")
-    p2 = input("Player 2: ")
+    p1 = acceptInput(1)
+    p2 = acceptInput(2)
     winner = calculateWinner(p1, p2)
     print(f"The winner is {winner}")
+
+
+def acceptInput(player_num):
+    player = input(f"Player {player_num}: ")
+    while validateChar(player) == False:
+        print("Invalid input, please enter your choice again")
+        player = input(f"Player {player_num}: ")
+
+    return player
+
+
+def validateChar(c):
+    if c in ['r', 'R', 'p', 'P', 's', 'S']:
+        return True
+    return False
 
 
 def calculateWinner(p1, p2):
