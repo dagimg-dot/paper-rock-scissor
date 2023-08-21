11f48d2c496e2a8b678031fe157bd28532a86b2d
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-08-21 08:59:45 +0300

    make the winning calculator algorithm more concise

--- main.js
+++ main.js
@@ -4,14 +4,10 @@ Winner = new Map();
 function populate() {
   // Populate the calculator map
   calculatorMap.set("rs", "r");
-  calculatorMap.set("pr", "p");
-  calculatorMap.set("sp", "s");
-  calculatorMap.set("sr", "r");
   calculatorMap.set("rp", "p");
-  calculatorMap.set("ps", "s");
+  calculatorMap.set("sp", "s");
 
   // Populate the winner map
-
   Winner.set("r", "Rock breaks scissor");
   Winner.set("p", "Paper covers rock");
   Winner.set("s", "Scissor cuts paper");
@@ -23,14 +19,14 @@ function welcome() {
 }
 
 function calculateWinner(p1, p2) {
-  return calculatorMap.get(p1 + p2);
+  return calculatorMap.get(p1 + p2) ||  calculatorMap.get(p2 + p1);
 }
 
 function main() {
   populate();
   welcome();
-  const p1 = "r"; // acceptInput(1)
-  const p2 = "s"; // acceptInput(2)
+  const p1 = "s"; // acceptInput(1)
+  const p2 = "p"; // acceptInput(2)
   const winner = calculateWinner(p1.toLowerCase(), p2.toLowerCase());
   console.log(Winner.get(winner));
 }
